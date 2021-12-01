0d61e345fde7481c6e5062de84c076d1a3b049fe
Daniel Groves <dan.groves@playsportsnetwork.com>
2021-12-01 18:02:18 +0000

    Adds AoC 2021-01-01

---
+++ 2021/01/01/main.js
@@ -0,0 +1,30 @@
+import readline from 'readline';
+import fs from 'fs';
+
+console.log('Starting');
+
+const input = 'input.txt';
+
+var lineReader = readline.createInterface({
+  input: fs.createReadStream(input)
+});
+
+console.log('Line Reader loaded');
+
+let prev = null;
+let count = 0;
+
+lineReader.on('line', function (line) {
+  const current = Number(line);
+
+  if (prev && prev < current) {
+    console.log(`${prev} is deeper than ${line}`)
+    count++;
+  }
+
+  prev = current;
+});
+
+lineReader.on('close', function() {
+  console.log(`It gets deeper ${count} times.`)
+})
